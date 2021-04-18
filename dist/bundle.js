e6053497150dc194572656b8296b13fc2364dbcf
Gustavo Raphael <gustavorapha3@hotmail.com>
2021-04-17 22:25:01 -0300

    Initial commit

---
+++ dist/bundle.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+
+var _App = require("./App");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
